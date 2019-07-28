7b15032b10dae63cd69ad8bf73ba71d21fea34f3
gili shrayber <gila@grth.co.il>
2019-07-28 15:42:09 +0300

    css if form and list

--- app/components/AddTask/Link.js
+++ app/components/AddTask/Link.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import Button from 'react-bootstrap/Button'
 const Link = ({ active, children, onClick }) => (
-  <button
+  <Button
     type="button"
     onClick={onClick}
     disabled={active}
@@ -11,7 +11,7 @@ const Link = ({ active, children, onClick }) => (
     }}
   >
     {children}
-  </button>
+  </Button>
 )
 
 Link.propTypes = {
